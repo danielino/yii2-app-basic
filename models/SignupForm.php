1beff3b6544972e7f58011067da8102113aa8220
Daniele Marcocci <daniele.marcocci@par-tec.it>
2017-02-28 00:06:41 +0100

    add admin lte layout

--- models/SignupForm.php
+++ models/SignupForm.php
@@ -24,13 +24,13 @@ class SignupForm extends Model
         return [
             ['username', 'filter', 'filter' => 'trim'],
             ['username', 'required'],
-            ['username', 'unique', 'targetClass' => '\common\models\User', 'message' => 'This username has already been taken.'],
+            ['username', 'unique', 'targetClass' => '\app\models\User', 'message' => 'This username has already been taken.'],
             ['username', 'string', 'min' => 2, 'max' => 255],
 
             ['email', 'filter', 'filter' => 'trim'],
             ['email', 'required'],
             ['email', 'email'],
-            ['email', 'unique', 'targetClass' => '\common\models\User', 'message' => 'This email address has already been taken.'],
+            ['email', 'unique', 'targetClass' => '\app\models\User', 'message' => 'This email address has already been taken.'],
 
             ['password', 'required'],
             ['password', 'string', 'min' => 6],
@@ -52,8 +52,6 @@ class SignupForm extends Model
             $user->setPassword($this->password);
             $user->generateAuthKey();
             $user->save();
-            
-            Yii::$app->session->set('tier2Timeout', Yii::$app->user->tier2Timeout);
             
             return $user;
         }
