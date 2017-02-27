b03644e4aa486373778706c7e9ac6e2fe0ce4c6b
Daniele Marcocci <daniele.marcocci@par-tec.it>
2017-02-27 23:51:43 +0100

    add resetPassword. fix htaccess

--- config/db.php
+++ config/db.php
@@ -1,8 +1,6 @@
 <?php
 
 return [
-    'mongodb' => [
-            'class' => '\yii\mongodb\Connection',
-            'dsn' => 'mongodb://developer:password@localhost:27017/mydatabase',
-        ],
+    'class' => '\yii\mongodb\Connection',
+    'dsn' => 'mongodb://developer:password@localhost:27017/mydatabase',
 ];
